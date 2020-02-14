7155d6f2c259834677d9457239d8175913945eb5
alexmspina <alexmspina@icloud.com>
2020-02-14 01:25:29 +0000

    add build and install stages to userRunsMusicworldDevContainer

--- acceptance/musicworld/rustdevcontainer_test.go
+++ acceptance/musicworld/rustdevcontainer_test.go
@@ -74,13 +74,26 @@ func aRunningShell() error {
 }
 
 func userRunsMusicworldSpinDevcontainer() error {
-	spinMusicworldDevContainer := exec.Command("musicworld", "spin", "devcontainer")
-	err := spinMusicworldDevContainer.Run()
+	
+	buildMusicworld := exec.Command("go", "build", "-o", "musicworld", "../../cmd/musicworld/main.go")
+	err := buildMusicworld.Run()
 	if err != nil {
 		return fmt.Errorf(
-			"error %v occurred while calling 'musicworld spin devcontainer'",
-			err,
-		)
+			"error %v trying to build musicworld", err)
+	}
+
+	installMusicworld := exec.Command("mv", "musicworld", "/go/bin/")
+	err = installMusicworld.Run()
+	if err != nil {
+		return fmt.Errorf(
+			"error %v occurred while installing (moving) musicworld ro /go/bin/", err)
+	}
+
+	spinMusicworldDevContainer := exec.Command("musicworld", "spin", "devcontainer")
+	err = spinMusicworldDevContainer.Run()
+	if err != nil {
+		return fmt.Errorf(
+			"error %v occurred while calling 'musicworld spin devcontainer'", err)
 	}
 
 	return nil
@@ -88,9 +101,7 @@ func userRunsMusicworldSpinDevcontainer() error {
 
 func thereIsARustdevContainerRunning() error {
 
-	checkForMusicworldDevContainer := exec.Command("docker", "exec", "-itd",
-		"musicworld_dev", "fish",
-	)
+	checkForMusicworldDevContainer := exec.Command("docker", "exec", "-itd", "musicworld_dev", "fish")
 
 	err := checkForMusicworldDevContainer.Run()
 	if err != nil {
